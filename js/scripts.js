1ceb70dd0a5d6b5c638b172fba7e9c3e63b30d43
on3g4i <d3887269@gmail.com>
2024-12-05 22:00:56 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function disponibilidade() {
+    let hora = document.getElementById('horas');
+    const data = new Date().getDay();
+     var estaAberto = false;
+    if (data>0 || data<6){
+        estaAberto=true;
+      
+    }
+}
